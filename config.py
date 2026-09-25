e49035009d17334ed3cef75a85f00614890e7558
Jurandir <jurandir007@users.noreply.github.com>
2026-09-25 23:05:41 +0100

    config: remove SQLite e dev-change-me fallbacks (fail-fast)

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-change-me")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
